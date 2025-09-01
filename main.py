4dfc27836ec1667cf02641d52cfe59846e73742e
Jeremy  Smith <jerem-mac@Jeremys-Air.lan>
2025-09-01 17:45:11 -0500

    Added music pause/play capabilities

--- main.py
+++ main.py
@@ -2,37 +2,46 @@ import sys
 import os
 import pygame
 
-POSSIBLE_MODES = ['args', 'file', 'input']
-MODE = "args"
-
-class Settings:
-    input_file: str | None
-    output_file: str | None
-    start_time: float | None
-    end_time: float | None
-
-    def __init__(self):
-        if MODE == "args":
-            pass
-        elif MODE == "file":
-            pass
-        elif MODE == "input":
-            pass
-
 class Timestamps:
     start: float
     end: float
     timestamps: list[float]
 
-
-
-
 file_name = sys.argv[1] if len(sys.argv) > 1 else "default_file.mp3"
 file_name = os.path.abspath(file_name)
 
+pygame.init()
+screen = pygame.display.set_mode((400, 300))
+
 pygame.mixer.init()
 pygame.mixer.music.load(file_name)
-pygame.mixer.music.play()
 
-while pygame.mixer.music.get_busy():
-    pygame.time.Clock().tick(10)
+pygame.font.init()
+font = pygame.font.Font(None, 36)
+pause_msg = font.render("press play to resume", True, (255, 255, 255))
+play_msg = font.render("press pause to stop", True, (255, 255, 255))
+
+paused = False
+
+pygame.mixer.music.play()
+while True:
+    for event in pygame.event.get():
+    
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                if paused:
+                    paused = False
+                    pygame.mixer.music.unpause()
+                    screen.fill((0, 0, 0))
+                    screen.blit(play_msg, (20, 20))
+                else:
+                    paused = True
+                    pygame.mixer.music.pause()
+                    screen.fill((0, 0, 0))
+                    screen.blit(pause_msg, (20, 20))
+        if event.type == pygame.QUIT:
+            pygame.mixer.music.stop()
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
+    pygame.time.Clock().tick(120)
