683e68c389b391c5edc70da67e0d5dd426547a7f
Jeremy  Smith <jeremy.ws.2008@gmail.com>
2025-09-09 09:18:53 -0500

    Added some makefile improvements

--- Makefile
+++ Makefile
@@ -4,4 +4,9 @@ main: main.c
 	
 
 clean: 
-	rm main
+	rm main 
+	rm -rf main.dSYM
+
+debug: main.c
+	gcc -g main.c -L/opt/homebrew/lib -L/opt/homebrew/opt/ffmpeg/lib -I/opt/homebrew/opt/ffmpeg/include -lavformat -lavcodec -lavutil -lswscale -lswresample -I/opt/homebrew/opt/sdl2/include -L/opt/homebrew/opt/sdl2/lib  -o main -Wall -Werror 
+	
--- main.c
+++ main.c
@@ -11,13 +11,17 @@
 
 int main(int argc, char *argv[]) {
 
+    if (argc != 2){
+        fprintf(stderr, "Usage: %s <filename>", argv[0]);
+        return 1;
+    }
+
+    const char* filename = argv[1];
+
     avformat_network_init();
 
-    
+    struct AudioFile* audio_file = AudioFile_from_file(filename);
+    AudioFile_destruct(audio_file);
 
     avformat_network_deinit();
-
-
-
-    
 }
